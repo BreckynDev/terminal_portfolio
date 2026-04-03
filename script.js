afd7916e1e5417e48092ebade72d9f441b111657
BIBS <breckynburke@gmail.com>
2026-04-03 12:25:25 -0600

    Added Style
    Made a full Stylesheet and added full function of command help and working on secret

--- script.js
+++ script.js
@@ -13,24 +13,43 @@ function initPrompt(terminal, command, output, input_line) {
             switch (command.value) {
                 case "help":
 
-                    textFreeze(terminal, command, output, input_line)
+                    textFreeze(terminal, command, output, input_line);
                     // Command response
-                    const response = document.createElement("p");
-                    response.textContent = "Testing help command";
-                    output.appendChild(response);
-                    newLine(terminal, command, output, input_line)
+                    const help_lines = ["about", "projects", "skills", "contact", "clear", "secret"];
+                    help_lines .forEach(lines => {
+                        const response = document.createElement("p");
+                        response.textContent = lines;
+                        output.appendChild(response);
+                    });
+                    newLine(terminal, command, output, input_line);
                     break;
+                case "secret":
+                    textFreeze(terminal, command, output, input_line);
+                    // Command response
+                    const info = document.createElement("p");
+                    info.textContent = "Try out some real commands";
+                    output.appendChild(info)
+                    const secret_lines = ["cd", "ls", "rm -rf /"];
+                    secret_lines.forEach(lines => {
+                        const response = document.createElement("p");
+                        response.textContent = lines;
+                        output.appendChild(response);
+                    });
+
+                    newLine(terminal, command, output, input_line);
+
             }
+            
         }
     }; 
-    command.addEventListener("keydown", handleKey)
+    command.addEventListener("keydown", handleKey);
 }   
 
 function textFreeze(terminal, command, output, input_line) {
     input_line.replaceChildren();
     const input = document.createElement("p");
     input.textContent = prompt_string + command.value;
-    input_line.appendChild(input)
+    input_line.appendChild(input);
 }
 function newLine(terminal, command, output, input_line) {
     const new_div = document.createElement("div");
@@ -38,16 +57,18 @@ function newLine(terminal, command, output, input_line) {
     const new_input = document.createElement("input");
     const new_output = document.createElement("output");
 
-    new_span.textContent = prompt_string
-    new_input.type = "text"
-    new_input.id = "command-input"
-    new_output.id = "output"
+    new_span.textContent = prompt_string;
+    new_input.type = "text";
+    new_input.className = "command-input";
+    new_output.className = "output";
 
     terminal.appendChild(new_div);
     new_div.appendChild(new_span);
     new_div.appendChild(new_input);
     terminal.appendChild(new_output);
-    initPrompt(terminal, new_input, new_output, new_div)
+    new_input.focus();
+
+    initPrompt(terminal, new_input, new_output, new_div);
 }
 
-initPrompt(terminal, command, output, input_line)
+initPrompt(terminal, command, output, input_line);
